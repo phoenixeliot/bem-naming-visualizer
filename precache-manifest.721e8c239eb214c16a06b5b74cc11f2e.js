c2f00daf4a63b4a4acc057b5f12eafa96ae173ea
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-09-21 18:57:09 -0600

    Updates

---
+++ precache-manifest.721e8c239eb214c16a06b5b74cc11f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a12c01f0d6f0080756de476dcbb0293",
+    "url": "/bem-naming-visualizer/index.html"
+  },
+  {
+    "revision": "7d1e40b26ff2322d88ae",
+    "url": "/bem-naming-visualizer/static/css/main.8102219c.chunk.css"
+  },
+  {
+    "revision": "801f128bab04abd6e9dd",
+    "url": "/bem-naming-visualizer/static/js/2.7fb1d1af.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bem-naming-visualizer/static/js/2.7fb1d1af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d1e40b26ff2322d88ae",
+    "url": "/bem-naming-visualizer/static/js/main.6cb48250.chunk.js"
+  },
+  {
+    "revision": "9b4ab5f5c8b10af99c50",
+    "url": "/bem-naming-visualizer/static/js/runtime-main.3bbc5e19.js"
+  }
+]);
