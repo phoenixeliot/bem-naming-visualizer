48c635c0de617e98e4d4cd6e7c532c3079b38353
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-11-08 10:26:08 -0700

    Add setting BEM string from URL hash

--- src/components/ColoredBemCode.js
+++ src/components/ColoredBemCode.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { setBemParts } from "features/bemParts/bemPartsSlice";
 import ContentEditable from "react-contenteditable";
+import { parseClass } from "./parseClass";
 
 const generateSpans = (parts) => {
   return [
@@ -11,22 +12,6 @@ const generateSpans = (parts) => {
     `<span class="code code_modifier">${parts.modifier || ""}</span>`,
     `<span class="code code_value">${parts.value || ""}</span>`,
   ].join("");
-};
-
-const keys = ["unknown", "block", "element", "modifier", "value"];
-
-const parseClass = (text) => {
-  const partsRegex = /^(?<block>[a-zA-Z0-9\\-]+)(?<element>__[a-zA-Z0-9\\-]*)?(?<modifier>_[a-zA-Z0-9\\-]*)?(?<value>_[a-zA-Z0-9\\-]*)?$/;
-  const match = text.match(partsRegex);
-  if (match === null) {
-    return { block: "", element: "", modifier: "", value: "", unknown: text };
-  }
-  const parts = match.groups;
-  const plainParts = Object.assign({}, parts); // Make it serializable
-  keys.forEach((key) => {
-    plainParts[key] = plainParts[key] || "";
-  });
-  return plainParts;
 };
 
 const ColoredBemCode = ({ parts, setBemParts }) => {
--- src/index.js
+++ src/index.js
@@ -6,9 +6,20 @@ import App from "./components/App";
 import rootReducer from "./reducers";
 import "./index.css";
 
+import { setBemParts } from "features/bemParts/bemPartsSlice";
+import { parseClass } from "./components/parseClass";
+
 const store = configureStore({
   reducer: rootReducer,
 });
+
+function updateFromHash() {
+  const initialBemParts = parseClass(window.location.hash.slice(1));
+  store.dispatch(setBemParts(initialBemParts));
+}
+
+window.addEventListener("hashchange", updateFromHash, false);
+updateFromHash();
 
 render(
   <Provider store={store}>
